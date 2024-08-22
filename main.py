ee71a6c850c064d9429080521eb0849afc5f7d6a
PC_8 <pc8@bluechain.kr>
2024-08-22 09:37:12 +0900

    update requirements요소추가

--- main.py
+++ main.py
@@ -1,22 +1,82 @@
-import json
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 import os
-from langchain.document_loaders import PyPDFDirectoryLoader
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chat_models import ChatOpenAI  # ChatOpenAI를 사용할 경우
-from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from openai import OpenAI
+import bs4
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+
+load_dotenv(
+    dotenv_path='.env',
+    verbose=True,
+)
+
+api_key = os.environ.get("OPEN_API_KEY")
+PROMPT_PATH = "prompt/qa_chain_prompt.txt"
+DATA_PATH = "ragData"
+document_loader = PyPDFDirectoryLoader(DATA_PATH)
+documents = document_loader.load()
 
 
+client = OpenAI(api_key=api_key)
+
+llm = ChatOpenAI(api_key=api_key, model_name="gpt-4o", temperature=0)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(documents)
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+retriever = vectorstore.as_retriever()
+
+# 파일 내용을 읽어서 system_prompt 변수에 저장
+# 파일 내용을 읽어서 system_prompt 변수에 저장 (UTF-8 인코딩 지정)
+with open(PROMPT_PATH, 'r', encoding='utf-8') as file:
+    system_prompt = file.read()
+    
+# print(system_prompt)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("user", "{question}"),
+    ]
+)
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 
-# 객체 내용 출력 유틸
-def show_json(obj):
-  display(json.loads(obj.model_dump_json()))
+# This Runnable takes a dict with keys 'input' and 'context',
+# formats them into a prompt, and generates a response.
+rag_chain_from_docs = (
+    {
+        "input": lambda x: x["input"],  # input query
+        "context": lambda x: format_docs(x["context"]),  # context
+    }
+    | prompt  # format query and context into prompt
+    | llm  # generate response
+    | StrOutputParser()  # coerce to string
+)
 
-def _get_response(thread_id):
-    return client.beta.threads.messages.list(thread_id=thread_id, order="asc")
+# Pass input query to retriever
+retrieve_docs = (lambda x: x["input"]) | retriever
 
-# Thread message 출력 유틸
-def print_message(thread_id):
-    for res in _get_response(thread_id):
-        print(f"[{res.role.upper()}]\n{res.content[0].text.value}\n")
+# Below, we chain `.assign` calls. This takes a dict and successively
+# adds keys-- "context" and "answer"-- where the value for each key
+# is determined by a Runnable. The Runnable operates on all existing
+# keys in the dict.
+chain = RunnablePassthrough.assign(context=retrieve_docs).assign(
+    answer=rag_chain_from_docs
+)
+
+chain.invoke({"input": "What is Task Decomposition"})
