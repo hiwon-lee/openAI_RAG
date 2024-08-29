af69ceb665b14f02ec22989c25c91de5c4b33150
PC_8 <pc8@bluechain.kr>
2024-08-29 09:32:33 +0900

    feat : flask연결

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def index():
     if request.method == 'POST':
         user_query = request.form['query']  # 사용자 입력 쿼리
         response = get_important_facts(user_query)  # GPT 응답 생성
+        
         return render_template('index.html', query=user_query, response=response)
     return render_template('index.html')
   
